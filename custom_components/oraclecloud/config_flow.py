0058e4dfc12881b330278067a16afe5ad8b5057b
Fabian Seitz <seitzf1@yahoo.de>
2026-05-11 12:25:02 +0200

    another fix for Github CI pytests - part 3

--- custom_components/oraclecloud/config_flow.py
+++ custom_components/oraclecloud/config_flow.py
@@ -124,6 +124,7 @@ class OracleCloudOptionsFlowHandler(OptionsFlow):
     def __init__(self, config_entry: ConfigEntry) -> None:
         """Initialize options flow."""
         self._config_entry = config_entry
+        self.handler = config_entry.entry_id
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
