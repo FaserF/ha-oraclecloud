b633dac5daa831aaa53dff20cd5a881977b9e05d
Fabian Seitz <fabian.seitz@pari.com>
2026-07-06 15:54:15 +0200

    fix: auto-install correct OCI SDK version on setup_entry

--- custom_components/oraclecloud/__init__.py
+++ custom_components/oraclecloud/__init__.py
@@ -20,6 +20,23 @@ PLATFORMS: list[Platform] = [
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Oracle Cloud Infrastructure from a config entry."""
+    def check_and_install_oci() -> None:
+        try:
+            import oci
+            if oci.__version__ == "2.181.0.post1":
+                return
+        except ImportError:
+            pass
+
+        import subprocess
+        import sys
+        subprocess.run([
+            sys.executable, "-m", "pip", "install", "--quiet", "--upgrade",
+            "git+https://github.com/FaserF/oci-python-sdk.git@28f956c93fd7d1718a5deaf49d25f900b2b280b4#oci"
+        ], check=True)
+
+    await hass.async_add_import_executor_job(check_and_install_oci)
+
     await hass.async_add_import_executor_job(
         importlib.import_module, "custom_components.oraclecloud.coordinator"
     )
